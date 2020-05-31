bdd469d2f0bf9805ee917d79f4630c4887b5cbe7
jiaxsun <joshsemedo@gmail.com>
2020-05-30 20:21:45 -0400

    Refactor string.format() to f-strings

--- start_the_box.py
+++ start_the_box.py
@@ -11,21 +11,21 @@ ip = input()
 host = name.lower() + ".htb"
 
 # Create the folder for the machine
-os.mkdir("/home/kali/htb/machines/{}".format(name))
+os.mkdir(f"/home/kali/htb/machines/{name}")
 
 # Add the boxname to /etc/hosts/
 with open("/etc/hosts", "a") as hosts_file:
-    hosts_file.write("{}\t{}".format(ip, host))
+    hosts_file.write(f"{ip}\t{host}")
     hosts_file.write("\n")
 
 # Set off some processes to run
 # Check if there's a website on the default port running, if so we can start there
-website = requests.get("http://{}".format(host))
+website = requests.get(f"http://{host}")
 # If there is a website, kick off gobuster
 # TODO: add support for dirb/dirbuster
 # TODO: add support for custom wordlist
 if website.status_code == 200:
-    print("There seems to be a website running on port 80 at {}.".format(ip))
+    print(f"There seems to be a website running on port 80 at {ip}.")
     print("Kicking off gobuster. Use custom wordlist? (default: /usr/share/dirbuster/directory-list-2.3-small.txt)")
     wl_path = input()
     if wl_path == "":
@@ -34,16 +34,16 @@ if website.status_code == 200:
 
     #TODO: fix this, looks like it stars process, but doesn't save output
     # Call gobuster with wordlist
-    subprocess.Popen(["gobuster", "dir", "--url", "http://{}".format(host), "-w", wordlist, "-o", "/home/kali/htb/machines/{}/gobuster_findings.txt".format(name)])
+    subprocess.Popen(["gobuster", "dir", "--url", f"http://{host}", "-w", wordlist, "-o", f"/home/kali/htb/machines/{name}/gobuster_findings.txt"])
 
 # Start a quick nmap looking for obvious services to start testing asap
-subprocess.Popen(["nmap", "--top-ports", "200", "-sS", ip, "-oN", "/home/kali/htb/machines/{}/quick_nmap_top200.txt".format(name)])
+subprocess.Popen(["nmap", "--top-ports", "200", "-sS", ip, "-oN", f"/home/kali/htb/machines/{name}/quick_nmap_top200.txt"])
 
 # Start a full scan in nice nmap output, which we can refer to easily
-subprocess.Popen(["nmap", "-p-", "-T4", ip, "-oN", "/home/kali/htb/machines/{}/nmap_full.txt".format(name)])
+subprocess.Popen(["nmap", "-p-", "-T4", ip, "-oN", f"/home/kali/htb/machines/{name}/nmap_full.txt"])
 
 # chmod directories
-box_path = "/home/kali/htb/machines/{}".format(name)
+box_path = f"/home/kali/htb/machines/{name}"
 for root, dirs, files in os.walk(box_path):
     for dir in dirs:
         os.chown(os.path.join(root, dir), 1000, 1000)
